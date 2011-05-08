26e820493afb9f39aa240a6925d617321806d6ac
IloveGirls <ilovegirls@ubuntu.ubuntu-domain>
2011-05-08 11:51:56 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '6f5bfaabb6046d5371b126623b49ece57aa70d4c9790a29db04a883a50c0d897498e2f995e32039041a789772efdb366bcebf8740b91ffcf3e7ad77283498d25'
